e8453b41f08519f031f062f33d3b3517d278fe99
Костя Лебедев <doroga12386@gmail.com>
2022-12-28 00:40:16 +0300

    The logic of work has been updated and new sorting options have been added

--- ActivityBot.py
+++ ActivityBot.py
@@ -2,7 +2,6 @@ import discord
 import sqlite3
 import configparser
 import time
-import datetime
 import asyncio
 from discord.ext import commands, tasks
 
@@ -18,6 +17,7 @@ TOKEN = config.get('config', 'TOKEN')
 timeout = int(config.get('config', 'timeout'))
 prefix = config.get('config', 'prefix')
 local = config.get('config', 'language')
+nums_lines = 7
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
@@ -50,9 +50,11 @@ async def db_update():
     activity = bot.get_guild(guild).get_member(user).activity
     if activity:
         try:
-            activity = activity.name
+            if activity.type == discord.ActivityType.playing:
+                activity = activity.name
         except:
             activity = str(activity)
+
         start_time = round(time.time())
         game_lst = []
         for game in cursor.execute(f"SELECT game FROM activity").fetchall():
@@ -62,7 +64,7 @@ async def db_update():
             cursor.execute("INSERT INTO activity VALUES(?,?,?)", sets)
             DB.commit()
         last_activity = activity
-        last_seconds = cursor.execute(f"SELECT seconds FROM activity where game = '{last_activity}'").fetchone()[0]
+        last_seconds = cursor.execute(f"SELECT seconds FROM activity where game = ?", (last_activity,)).fetchone()[0]
         while True:
             try:
                 activity = bot.get_guild(guild).get_member(user).activity.name
@@ -78,18 +80,18 @@ async def db_update():
             if minutes < 10: minutes = f'0{minutes}'
             if hours < 10: hours = f'0{hours}'
             full_time = f'{hours}:{minutes}:{seconds}'
-            cursor.execute(f"Update activity set time = '{full_time}' where game = '{last_activity}'")
-            cursor.execute(f"Update activity set seconds = {sec + last_seconds} where game = '{last_activity}'")
+            cursor.execute(f"Update activity set time = ? where game = ?", (full_time, last_activity,))
+            cursor.execute(f"Update activity set seconds = ? where game = ?", (sec + last_seconds, last_activity,))
             DB.commit()
             await asyncio.sleep(timeout)
 
 
-@bot.command(name='time')
-async def _time(ctx):
+def get_DB(sort: str):
     seconds = 0
-    embed = discord.Embed(title=L10N.get(local, 'game_time'), colour=discord.Colour.green())
-    for game in cursor.execute(f"SELECT * FROM activity").fetchall():
-        embed.add_field(name=f'*{game[0]}*', value=game[2], inline=False)
+    type = 'game' if sort == 'game' else 'seconds DESC'
+    list_games = []
+    for game in cursor.execute(f"SELECT * FROM activity ORDER BY {type}").fetchall():
+        list_games.append(game)
         seconds += game[1]
     hours, seconds = divmod(seconds, 60 * 60)
     minutes, seconds = divmod(seconds, 60)
@@ -97,8 +99,125 @@ async def _time(ctx):
     if minutes < 10: minutes = f'0{minutes}'
     if hours < 10: hours = f'0{hours}'
     all_time = f'{hours}:{minutes}:{seconds}'
-    embed.set_footer(text=L10N.get(local, 'total_time').format(all_time))
-    await ctx.send(embed=embed)
+    return list_games, all_time
+
+
+@bot.command(name='time')
+async def _time(ctx):
+    ctx.typing()
+
+    global cur_page
+    global sort
+    sort = 'time'
+    cur_page = 0
+
+    games_count = 8
+    pgs_content = []
+    total_games = len(get_DB(sort)[0])
+    while True:
+        if total_games < 1:
+            break
+        if total_games <= games_count:
+            pgs_content.append(total_games)
+            break
+        else:
+            pgs_content.append(games_count)
+            total_games -= games_count
+    pages = len(pgs_content)
+
+    async def peginator(sort):
+        games = get_DB(sort)
+        embed = discord.Embed(title=L10N.get(local, 'game_time'),
+                              description=L10N.get(local, 'total_time').format(games[1]),
+                              colour=discord.Colour.green())
+        for x in range(pgs_content[cur_page]):
+            num = cur_page * games_count + x
+            embed.add_field(name=f'{num + 1}) {games[0][num][0]}', value=f'`{games[0][num][2]}`', inline=False)
+        embed.set_footer(text=L10N.get(local, 'cur_page').format(cur_page + 1, pages))
+        return embed
+
+    class Counter(discord.ui.View):
+        def __init__(self):
+            super().__init__()
+
+        @discord.ui.button(label="", style=discord.ButtonStyle.gray, emoji="⏮️")
+        async def first(self, interaction: discord.Interaction, button: discord.ui.Button):
+            global cur_page
+            if ctx.author.id == interaction.user.id:
+                cur_page = 0
+                await message.edit(embed=await peginator(sort))
+            try:
+                await interaction.response.send_message()
+            except:
+                pass
+
+        @discord.ui.button(label="", style=discord.ButtonStyle.gray, emoji="◀️")
+        async def prev(self, interaction: discord.Interaction, button: discord.ui.Button):
+            global cur_page
+            if ctx.author.id == interaction.user.id:
+                if cur_page > 0:
+                    cur_page -= 1
+                    await message.edit(embed=await peginator(sort))
+            try:
+                await interaction.response.send_message()
+            except:
+                pass
+
+        @discord.ui.button(label="", style=discord.ButtonStyle.gray, emoji="❌")
+        async def stop(self, interaction: discord.Interaction, button: discord.ui.Button):
+            if ctx.author.id == interaction.user.id:
+                await message.delete()
+            try:
+                await interaction.response.send_message()
+            except:
+                pass
+
+        @discord.ui.button(label="", style=discord.ButtonStyle.gray, emoji="▶️")
+        async def next(self, interaction: discord.Interaction, button: discord.ui.Button):
+            global cur_page
+            if ctx.author.id == interaction.user.id:
+                if cur_page < pages - 1:
+                    cur_page += 1
+                    await message.edit(embed=await peginator(sort))
+            try:
+                await interaction.response.send_message()
+            except:
+                pass
+
+        @discord.ui.button(label="", style=discord.ButtonStyle.gray, emoji="⏭️")
+        async def last(self, interaction: discord.Interaction, button: discord.ui.Button):
+            global cur_page
+            if ctx.author.id == interaction.user.id:
+                cur_page = pages - 1
+                await message.edit(embed=await peginator(sort))
+            try:
+                await interaction.response.send_message()
+            except:
+                pass
+
+        @discord.ui.button(label=L10N.get(local, 'sort_time'), style=discord.ButtonStyle.green, emoji="🕐", row=1)
+        async def _sort_time(self, interaction: discord.Interaction, button: discord.ui.Button):
+            global cur_page
+            if ctx.author.id == interaction.user.id:
+                sort = 'time'
+                await message.edit(embed=await peginator(sort))
+            try:
+                await interaction.response.send_message()
+            except:
+                pass
+
+        @discord.ui.button(label=L10N.get(local, 'sort_game'), style=discord.ButtonStyle.green, emoji="🔠", row=1)
+        async def _sort_game(self, interaction: discord.Interaction, button: discord.ui.Button):
+            global cur_page
+            if ctx.author.id == interaction.user.id:
+                sort = 'game'
+                await message.edit(embed=await peginator(sort))
+            try:
+                await interaction.response.send_message()
+            except:
+                pass
+
+    message = await ctx.send(embed=await peginator(sort), view=Counter())
 
 
 bot.run(TOKEN)
